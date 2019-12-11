0e7581ddd3829633e001708994e79f602535e2a1
Alex <alexgrantjnr@yahoo.co.uk>
2019-12-11 21:03:40 +0000

    Project creation Initial Commit

---
+++ src/test/java/com/alex/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
